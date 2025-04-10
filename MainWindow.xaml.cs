6fc4485a101f44a0b986c407d39b150cf53b6367
DevMHfi <7501123max@gmail.com>
2025-04-10 13:17:03 +0300

    new enter

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -30,6 +30,7 @@ namespace uprspo18
 			var response = client.DownloadString("http://127.0.0.1:63450/api/hotels");
 			CurrentHotels = JsonConvert.DeserializeObject<List<Hotel>>(response);
 			DataContext = this;
+
 		}
 	}
 }
